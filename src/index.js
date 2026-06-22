66e9e21d19409bb6e7ed040eb034631a96da2b3e
Jassiel Ovando <jassielovando@protonmail.com>
2026-06-21 22:12:32 -0400

    better

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import { parse } from "@jassielof/zon";
 const ZIGLANG_ORG = "https://ziglang.org/";
 
 const DOWNLOAD_MIRRORS = ZIGLANG_ORG + "download/community-mirrors.txt";
+_ = DOWNLOAD_MIRRORS;
+_ = parse;
